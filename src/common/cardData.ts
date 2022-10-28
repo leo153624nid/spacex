d9561cb9f5c72956c1802b3ac57affc6cbde89fd
leo153624nid <barjastroy@gmail.com>
2022-10-28 18:42:22 +0300

    Create cardData.ts

---
+++ src/common/cardData.ts
@@ -0,0 +1,25 @@
+// Данные для каточек с информацией о компании и полете
+const cardData = [
+    {
+        mode: 'order',
+        textOver: 'мы',
+        textUnder: 'на рынке',
+    },
+    {
+        mode: 'garant',
+        textOver: 'гарантируем',
+        textUnder: 'безопасность',
+    },
+    {
+        mode: 'year',
+        textOver: 'календарик за',
+        textUnder: 'в подарок',
+    },
+    {
+        mode: 'duration',
+        textOver: 'путешествие',
+        textUnder: 'дней',
+    },
+]
+
+export default cardData
