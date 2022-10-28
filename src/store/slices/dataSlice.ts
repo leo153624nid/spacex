bd2a2a079af4a78e8485faa332b2051549d0b0c9
leo153624nid <barjastroy@gmail.com>
2022-10-28 17:30:18 +0300

    Create dataSlice.ts

---
+++ src/store/slices/dataSlice.ts
@@ -0,0 +1,39 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+export interface InitState {
+    order: number | null
+    garant: string | null
+    year: number | null
+    duration: number | null
+}
+
+const initialState: InitState = {
+    order: null,
+    garant: null,
+    year: null,
+    duration: null,
+}
+
+const dataSlice = createSlice({
+    name: 'data',
+    initialState,
+    reducers: {
+        setData(state, action: PayloadAction<InitState>) {
+            state.order = action.payload.order
+            state.garant = action.payload.garant
+            state.year = action.payload.year
+            state.duration = action.payload.duration
+        },
+        removeData(state) {
+            state.order = null
+            state.garant = null
+            state.year = null
+            state.duration = null
+        },
+    },
+})
+
+export const { setData, removeData } = dataSlice.actions
+
+export default dataSlice.reducer
