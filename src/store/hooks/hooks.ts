02e98416c1ea6d4d7c5f18c10723a473cdd7877a
leo153624nid <barjastroy@gmail.com>
2022-10-28 17:30:13 +0300

    Create hooks.ts

---
+++ src/store/hooks/hooks.ts
@@ -0,0 +1,6 @@
+import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux'
+
+import type { RootState, AppDispatch } from '../reduxStore'
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
