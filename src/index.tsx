b1cd8d32e76759a15f49c9b46d790a181456fb70
leo153624nid <barjastroy@gmail.com>
2022-10-28 17:30:03 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,19 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.scss'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import store from './store/reduxStore'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
     </React.StrictMode>
 )
