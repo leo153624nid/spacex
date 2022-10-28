d7cc892d14d8043b5a3ea99ec6f23edc3d1e8f2c
leo153624nid <barjastroy@gmail.com>
2022-10-28 17:29:50 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import dataAPI from './api/dataAPI'
 import s from './App.module.scss'
+import HomePage from './pages/HomePage/HomePage'
+import NewTripPage from './pages/NewTripPage/NewTripPage'
+import { useAppDispatch } from './store/hooks/hooks'
+import { InitState, setData } from './store/slices/dataSlice'
 
 function App() {
+    const dispatch = useAppDispatch()
+
+    // Получаем данные при загрузке страницы
+    useEffect(() => {
+        dataAPI
+            .getData()
+            .then((data: InitState) => {
+                dispatch(
+                    setData({
+                        order: data.order,
+                        garant: data.garant,
+                        year: data.year,
+                        duration: data.duration,
+                    })
+                )
+            })
+            .catch((error) => {
+                console.log(error)
+                alert('Данные не получены')
+            })
+    }, [dispatch])
     return (
         <div className={s.App}>
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/newtrip" element={<NewTripPage />} />
+            </Routes>
         </div>
     )
 }
