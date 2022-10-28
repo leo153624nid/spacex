a7e98a800608a0b1d9b10d1f316424e886d69e0e
leo153624nid <barjastroy@gmail.com>
2022-10-28 18:59:16 +0300

    Update Body.tsx

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
-import StartButton from '../Button/StartButton'
+import StartButton from '../StartButton/StartButton'
 import Card from '../Card/Card'
 import s from './Body.module.scss'
 
@@ -14,18 +14,21 @@ interface BodyProps {
 }
 
 function Body({ order, garant, year, duration }: BodyProps) {
+    const startTrip = () => {}
     return (
         <div className={s.body}>
             <div className={s.sideLeft}>
                 sideLeft
-                <StartButton />
+                <StartButton color="orange" onClick={startTrip}>
+                    Начать путешествие
+                </StartButton>
             </div>
 
             <div className={s.sideRight}>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card mode="order" data={order} />
+                <Card mode="garant" data={garant} />
+                <Card mode="year" data={year} />
+                <Card mode="duration" data={duration} />
             </div>
         </div>
     )
