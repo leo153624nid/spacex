e7879167bf25933ab48c4efe6ec3f215db5edf57
leo153624nid <barjastroy@gmail.com>
2022-10-28 19:10:29 +0300

    Update NewTripPage.tsx

--- src/pages/NewTripPage/NewTripPage.tsx
+++ src/pages/NewTripPage/NewTripPage.tsx
@@ -2,14 +2,26 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import StartButton from '../../components/StartButton/StartButton'
 import { useAppSelector } from '../../store/hooks/hooks'
 import s from './NewTripPage.module.scss'
 
 function NewTrip() {
+    const navigate = useNavigate()
     const data = useAppSelector((state) => state.data)
 
+    const goHome = () => {
+        navigate('/', { replace: true })
+    }
+
     return data ? (
-        <div className={s.homePage}>11111</div>
+        <div className={s.homePage}>
+            NEW TRIP
+            <StartButton mode="home" onClick={goHome}>
+                Вернуться на главную
+            </StartButton>
+        </div>
     ) : (
         <div className={s.homePage}>
             <div className={s.loading}>
