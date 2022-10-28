c4820a306a7c41215c6d8cc3cc6ad6d2d0fb1976
leo153624nid <barjastroy@gmail.com>
2022-10-28 18:42:35 +0300

    Update NewTripPage.tsx

--- src/pages/NewTripPage/NewTripPage.tsx
+++ src/pages/NewTripPage/NewTripPage.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import React from 'react'
 import { useAppSelector } from '../../store/hooks/hooks'
-import s from './NewTrip.module.scss'
+import s from './NewTripPage.module.scss'
 
 function NewTrip() {
-    const { data } = useAppSelector((state) => state.data)
+    const data = useAppSelector((state) => state.data)
 
     return data ? (
         <div className={s.homePage}>11111</div>
