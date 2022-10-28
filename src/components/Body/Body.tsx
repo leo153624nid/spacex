eebd386960b0d6f403060d1294bc8688583fbf38
leo153624nid <barjastroy@gmail.com>
2022-10-28 18:42:24 +0300

    Update Body.tsx

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,4 +1,9 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react'
+import StartButton from '../Button/StartButton'
+import Card from '../Card/Card'
 import s from './Body.module.scss'
 
 interface BodyProps {
@@ -10,49 +15,19 @@ interface BodyProps {
 
 function Body({ order, garant, year, duration }: BodyProps) {
     return (
-        <header className={s.header}>
-            <div className={s.banner}>
-                <img src={banner} alt="banner" />
+        <div className={s.body}>
+            <div className={s.sideLeft}>
+                sideLeft
+                <StartButton />
             </div>
 
-            <div className={s.content}>
-                <div className={s.avatar}>
-                    <img src={avatar} alt="avatar" />
-                </div>
-
-                <div className={s.userData}>
-                    <div className={s.userName}>
-                        <span>{userName}</span>
-                    </div>
-
-                    <div className={s.userButtons}>
-                        <div className={s.btn}>
-                            <div className={s.icon}>
-                                <img
-                                    src={email}
-                                    alt="email"
-                                    className={s.email}
-                                />
-                            </div>
-
-                            <span className={s.text}>Message</span>
-                        </div>
-
-                        <div className={s.btn}>
-                            <div className={s.icon}>
-                                <img
-                                    src={phone}
-                                    alt="phone"
-                                    className={s.phone}
-                                />
-                            </div>
-
-                            <span className={s.text}>Call</span>
-                        </div>
-                    </div>
-                </div>
+            <div className={s.sideRight}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
             </div>
-        </header>
+        </div>
     )
 }
 
