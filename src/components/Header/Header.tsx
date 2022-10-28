9d4b25cdd0c11e46d4878ed664f3cce54825ec77
leo153624nid <barjastroy@gmail.com>
2022-10-28 17:29:57 +0300

    Create Header.tsx

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import s from './Header.module.scss'
+
+interface HeaderProps {
+    order: number | null
+    garant: string | null
+    year: number | null
+    duration: number | null
+}
+
+function Header({ order, garant, year, duration }: HeaderProps) {
+    return (
+        <header className={s.header}>
+            <div className={s.banner}>
+                <img src={banner} alt="banner" />
+            </div>
+
+            <div className={s.content}>
+                <div className={s.avatar}>
+                    <img src={avatar} alt="avatar" />
+                </div>
+
+                <div className={s.userData}>
+                    <div className={s.userName}>
+                        <span>{userName}</span>
+                    </div>
+
+                    <div className={s.userButtons}>
+                        <div className={s.btn}>
+                            <div className={s.icon}>
+                                <img
+                                    src={email}
+                                    alt="email"
+                                    className={s.email}
+                                />
+                            </div>
+
+                            <span className={s.text}>Message</span>
+                        </div>
+
+                        <div className={s.btn}>
+                            <div className={s.icon}>
+                                <img
+                                    src={phone}
+                                    alt="phone"
+                                    className={s.phone}
+                                />
+                            </div>
+
+                            <span className={s.text}>Call</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
