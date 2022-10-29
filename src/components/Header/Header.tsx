a02cdf0e09044937a3275c6a3a4c22e6f93c9701
leo153624nid <barjastroy@gmail.com>
2022-10-29 12:52:01 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,33 +6,35 @@ import logo from '../../img/spacex-logo.png'
 function Header() {
     return (
         <header className={s.header}>
-            <div className={s.logo}>
-                <img src={logo} alt="logo" />
-            </div>
-
-            <div className={s.content}>
-                <div className={s.text}>
-                    <span>Главная</span>
+            <div className={s.header_container}>
+                <div className={s.logo}>
+                    <img src={logo} alt="logo" />
                 </div>
 
-                <div className={s.text}>
-                    <span>Технология</span>
-                </div>
+                <div className={s.content}>
+                    <div className={s.text}>
+                        <span>Главная</span>
+                    </div>
 
-                <div className={s.text}>
-                    <span>График полетов</span>
-                </div>
+                    <div className={s.text}>
+                        <span>Технология</span>
+                    </div>
 
-                <div className={s.text}>
-                    <span>Гарантии</span>
-                </div>
+                    <div className={s.text}>
+                        <span>График полетов</span>
+                    </div>
 
-                <div className={s.text}>
-                    <span>О компании</span>
-                </div>
+                    <div className={s.text}>
+                        <span>Гарантии</span>
+                    </div>
 
-                <div className={s.text}>
-                    <span>Контакты</span>
+                    <div className={s.text}>
+                        <span>О компании</span>
+                    </div>
+
+                    <div className={s.text}>
+                        <span>Контакты</span>
+                    </div>
                 </div>
             </div>
         </header>
